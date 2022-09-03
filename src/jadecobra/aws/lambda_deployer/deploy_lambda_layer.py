858228fc3a02e2d4dd203c5f047b4f05fa39df39
jadecobra <jakeitegsy@yahoo.com>
2022-09-02 23:41:36 -0500

    update directory

--- src/jadecobra/aws/lambda_deployer/deploy_lambda_layer.py
+++ src/jadecobra/aws/lambda_deployer/deploy_lambda_layer.py
@@ -55,7 +55,7 @@ class LambdaLayer(deploy_lambda.LambdaDeployer):
             print(f"{message}Success")
 
     def directory(self):
-        return "lambda_layers"
+        return f"lambda_layers/{self.name}"
 
     def runtime(self):
         return "python"
@@ -64,7 +64,7 @@ class LambdaLayer(deploy_lambda.LambdaDeployer):
         try:
             for dependency in self.dependencies:
                 os.system(
-                    f"pip install --target ./{self.directory()}/{self.name}/{self.runtime()} {dependency} --upgrade"
+                    f"pip install --target ./{self.directory()}/{self.runtime()} {dependency} --upgrade"
                 )
         except TypeError:
             print("No dependencies to install")
