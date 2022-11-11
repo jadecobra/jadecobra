87c9eab96ac147940e68356b661bd19ada1dee83
jadecobra <jakeitegsy@yahoo.com>
2022-11-11 13:13:18 -0600

    update tests

--- tests/test_z_build_publish.py
+++ tests/test_z_build_publish.py
@@ -1,7 +1,7 @@
 import src.jadecobra.toolkit
 
 
-class TestBuildDeploy(src.jadecobra.toolkit.TestCase):
+class TestZBuildDeploy(src.jadecobra.toolkit.TestCase):
 
-    def test_build_and_publish(self):
+    def test_z_build_and_publish(self):
         self.build_and_publish()
