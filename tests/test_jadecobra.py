e574ac55e22d5df77e984bdb3162a2a41465d182
jadecobra <jakeitegsy@yahoo.com>
2023-03-31 15:44:00 -0500

    test dependencies

--- tests/test_jadecobra.py
+++ tests/test_jadecobra.py
@@ -185,5 +185,5 @@ class TestJadeCobra(src.jadecobra.toolkit.TestCase):
             src.jadecobra.toolkit.get_pyproject_version(
                 src.jadecobra.toolkit.get_pyproject()
             ),
-            ('0.2.', '2')
+            ('0.2.', '3')
         )
