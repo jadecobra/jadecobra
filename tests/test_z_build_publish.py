a20117a6b279d1567572df25372a083d7ecc5178
jadecobra <jakeitegsy@yahoo.com>
2023-04-02 21:58:24 -0500

    add condition

--- tests/test_z_build_publish.py
+++ tests/test_z_build_publish.py
@@ -19,17 +19,18 @@ class TestZBuildDeploy(src.jadecobra.tester.TestCase):
             os.system(f'pip {command}')
 
     def assert_published_version_is_source_version(self):
-        self.get_latest_published_version()
-        import jadecobra
-        importlib.reload(jadecobra)
-        self.assertEqual(
-            jadecobra.__version__,
-            src.jadecobra.__version__
-        )
-        self.assertEqual(
-            jadecobra.__version__,
-            self.version.current_pyproject_version
-        )
+        if src.jadecobra.toolkit.git_diff():
+            self.get_latest_published_version()
+            import jadecobra
+            importlib.reload(jadecobra)
+            self.assertEqual(
+                jadecobra.__version__,
+                src.jadecobra.__version__
+            )
+            self.assertEqual(
+                jadecobra.__version__,
+                self.version.current_pyproject_version
+            )
 
     def test_z_published_version_is_test_version(self):
         self.assert_published_version_is_source_version()
