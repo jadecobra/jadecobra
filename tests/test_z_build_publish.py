c4cd9506795b0ddda63e733b947e752229880b66
jadecobra <jakeitegsy@yahoo.com>
2023-04-19 19:49:03 -0500

    add test

--- tests/test_z_build_publish.py
+++ tests/test_z_build_publish.py
@@ -24,6 +24,7 @@ class TestZBuildDeploy(src.jadecobra.tester.TestCase):
     def test_z_published_version_is_test_version(self):
         result = src.jadecobra.toolkit.publish(True)
         if result and 'ERROR' in result.stdout.decode():
+            'testing 123'
             # versioning.Version().update()
             self.assertFalse(True)
             # src.jadecobra.publish(True)
